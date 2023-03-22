d4b0daf95f39f87d0cefe9ddd2bc35af0be9b79a
swj4664 <108079743+swj4664@users.noreply.github.com>
2023-03-23 00:38:11 +0900

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -11,11 +11,11 @@ function onGeoError() {
     alert("날씨를 제공할 위치를 찾을 수 없습니다.")
 }
 
-function getWeather(lat, lon){
+function getWeather(lat, lon) {
     $.ajax({
         url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=c2d0e5bbe716ac60f8541d884dda4c9c&lang=${lang}`,
         type: "GET",
-        data: {units:"metric"}, // 섭씨로 변환
+        data: { units: "metric" }, // 섭씨로 변환
         success: function (data) {
             Info(data)// temp, weather
             menuInfo(data)
@@ -26,40 +26,68 @@ function getWeather(lat, lon){
     });
 }
 
-    function Info(data) {
+function Info(data) {
     let weatherGet = data.weather[0].description
     let tempUp = Math.ceil(data.main.temp * 10) / 10; // 반올림
     let temp = ''
     let weather = ''
     let icon = ''
 
-    if(weatherGet=='맑음'){
+    if (weatherGet == '맑음') {
         icon += `<img src="./img/sun.png" alt="" style="width:25px; height:25px; position:absolute;">`
     }
-    
-    temp += `<span>현재온도는 ${tempUp}°C 입니다</span>`
-    weather += `<span>현재 날씨는 ${weatherGet} ${icon} <br>입니다</span>`
+
+    temp += `<span>현재온도는 ${tempUp}°C</span>`
+    weather += `<span>날씨는 '${weatherGet}'입니다</span>`
     $('.tempInfo').append(temp)
     $('.weatherInfo').append(weather)
 }
-
-function menuInfo(data) {
-    let weatherIcon = data.weather[0].icon
-    if(weatherIcon == '03d'){
-        
-    }
-}
-
 
 
 let icon = document.querySelectorAll('.list')
 let indi = document.querySelector('.indicator')
 
-function menuAct(e){
+
+//현재 날씨 메뉴에 표시
+function menuInfo(data) {
+    let weatherIcon = data.weather[0].icon
+    if (weatherIcon == '01d' || weatherIcon == '01n') {
+        icon.forEach((i) =>
+            i.classList.remove("active"))
+        icon[0].classList.add("active")
+    } else if (weatherIcon == '02d' || weatherIcon == '02n') {
+        icon.forEach((i) =>
+            i.classList.remove("active"))
+        icon[1].classList.add("active")
+    } else if (weatherIcon == '03d' || weatherIcon == '03n' || weatherIcon == '04d' || weatherIcon == '04n') {
+        icon.forEach((i) =>
+            i.classList.remove("active"))
+        icon[2].classList.add("active")
+    } else if (weatherIcon == '09d' || weatherIcon == '09n' || weatherIcon == '10d' || weatherIcon == '10n') {
+        icon.forEach((i) =>
+            i.classList.remove("active"))
+        icon[3].classList.add("active")
+    } else if (weatherIcon == '11d' || weatherIcon == '11n') {
+        icon.forEach((i) =>
+            i.classList.remove("active"))
+        icon[4].classList.add("active")
+    } else if (weatherIcon == '13d' || weatherIcon == '13n') {
+        icon.forEach((i) =>
+            i.classList.remove("active"))
+        icon[5].classList.add("active")
+    } else if (weatherIcon == '50d' || weatherIcon == '50n') {
+        icon.forEach((i) =>
+            i.classList.remove("active"))
+        icon[6].classList.add("active")
+    }
+}
+
+
+function menuAct(e) {
     icon.forEach((i) =>
-    i.classList.remove("active"))
+        i.classList.remove("active"))
     this.classList.add("active")
     e.preventDefault();
 }
-icon.forEach((i)=>
-i.addEventListener('click', menuAct))
+icon.forEach((i) =>
+    i.addEventListener('click', menuAct))
