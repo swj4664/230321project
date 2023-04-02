6ddd041a92cf0ba3d03034c87b5dd33946a2564b
swj4664 <108079743+swj4664@users.noreply.github.com>
2023-04-02 20:29:42 +0900

    Add files via upload

--- js/open.js
+++ js/open.js
@@ -1,10 +1,4 @@
 if (window.innerWidth <= 600) {
-    let textSize = document.querySelectorAll('.hello > a')
-    let textSize2 = document.querySelector('.hello2 a')
-    textSize.forEach((e) =>
-        e.classList.remove("active"))
-    textSize[1].classList.add("active")
-
-    setTimeout(function(){textSize2.classList.add("active")}, 2000)
-    
+    // let textSize2 = document.querySelector('.hello2 a')
+    // setTimeout(function(){textSize2.classList.add("active")},2000)
 }
