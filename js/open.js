3d74ba3c8504bb950012090353d72c8d5b7f0e5f
swj4664 <108079743+swj4664@users.noreply.github.com>
2023-06-04 18:59:49 +0900

    Add files via upload

--- js/open.js
+++ js/open.js
@@ -2,3 +2,7 @@ if (window.innerWidth <= 600) {
     // let textSize2 = document.querySelector('.hello2 a')
     // setTimeout(function(){textSize2.classList.add("active")},2000)
 }
+
+setTimeout(function() {
+    window.location.href = "weather.html";
+  }, 4000);
